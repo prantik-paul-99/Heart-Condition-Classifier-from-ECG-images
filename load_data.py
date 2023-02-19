23d5efa30d3be05e90807c6e13933cc38bff8e25
Prantik Paul <1705071@ugrad.cse.buet.ac.bd>
2023-02-19 18:18:04 +0600

    Processing with pillow

--- load_data.py
+++ load_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 import cv2 as cv
 import PIL as pil
+from PIL import ImageFilter, ImageOps, Image
 
 folders = [
           'dataset_cropped/Covid',
@@ -27,19 +28,29 @@ def load_data():
         image_paths = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.png')]
         # print("len of image_paths: {}".format(len(image_paths)))
         for image_path in image_paths:
-            img = cv.imread(image_path)
+            # img = cv.imread(image_path)
+            img = Image.open(image_path)
             if img is not None:
-                gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-                invert = cv.bitwise_not(gray)
-                dilate = cv.dilate(invert, None, iterations=2)
+                # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+                gray = ImageOps.grayscale(img)
+                # gray = img
+                # invert = cv.bitwise_not(gray)
+                invert = ImageOps.invert(gray)
+                # dilate = cv.dilate(invert, None, iterations=2)
+                dilate = invert.filter(ImageFilter.MaxFilter(3))
                 # erode = cv.erode(dilate, None, iterations=2)
 
-                img = cv.resize(dilate, (224, 224))
+                img = dilate.resize((28,28))
                 img = np.array(img)
                 
                 images.append(img)
+                
         
         images = np.array(images)
+
+        print("images shape: {}".format(images.shape))
+        print("images[0] shape: {}".format(images[0].shape))
+
         print("done loading images from {}".format(folder_path))
         print("total images loaded: {}".format(len(images)))
 
